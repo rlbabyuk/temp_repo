ef9b01fd1982442b807e755f0fe477baad3e7a01
Ruslana <rbabyuk@redhat.com>
2018-05-16 11:38:18 +0300

    fff

--- tests/test_file.py
+++ tests/test_file.py
@@ -6,7 +6,7 @@ from ui_map import home_page_elements
 def test_file():
     home = home_page.HomePage()
     home.wait_for_home_page()
-    home.select_from_dropdown(home_page_elements.doc_convert_dropdown.element(), "Convert to DOC")
+    home.select_from_dropdown(home_page_elements.doc_convert_dropdown.element(), "Convert to DOCuiui")
     home.wait_for_element(home_page_elements.file_upload.element())
     # autoit.win_wait_active("File Upload", 5)
     # autoit.send(os.path.join(mpath, "1.jpg"))
